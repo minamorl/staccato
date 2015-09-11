5bda7778339798475ad477dd08aac022d945dab8
minamorl <minamorl@users.noreply.github.com>
2015-09-11 16:02:12 +0900

    Bump version: 0.0.3 → 0.0.4

--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ class Tox(TestCommand):
 
 setup(
     name="staccato",
-    version="0.0.3",
+    version="0.0.4",
     packages=find_packages(),
     install_requires = ['requests', 'requests_oauthlib'],
     author = "minamorl",
