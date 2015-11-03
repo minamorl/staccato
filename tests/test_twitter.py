6f159050a6eb9b16ac092649f0b77b0d1e4ada07
minamorl <minamorl@users.noreply.github.com>
2015-11-03 09:40:34 +0900

    fix test

--- tests/test_twitter.py
+++ tests/test_twitter.py
@@ -3,23 +3,18 @@ import os
 import itertools
 
 
-def test_update_status(api):
-    res = api.statuses_update(status="@tos status")
-    print(res)
-    api.statuses_destroy(res['id'])
-
-
 def test_account_verify_credentials(api):
     assert api.account_verify_credentials()['screen_name'] == os.environ["SCREEN_NAME"]
 
 
 def test_get_followers(api):
-    print(api.followers_ids(screen_name=os.environ["SCREEN_NAME"]))
+    api.followers_ids(screen_name=os.environ["SCREEN_NAME"])
 
 
 def test_get_followings(api):
-    print(api.friends_ids(screen_name=os.environ["SCREEN_NAME"]))
+    api.friends_ids(screen_name=os.environ["SCREEN_NAME"])
+
 
 def test_user_stream(api):
     for parsed in itertools.islice(api.user_stream(), 10):
-        print(parsed)
+        pass
